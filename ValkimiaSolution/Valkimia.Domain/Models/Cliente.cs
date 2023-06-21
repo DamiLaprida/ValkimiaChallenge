5d60b098749ae8bd69423507a79b41e303c6bb68
DamianIanniruberto <damian.ianniruberto@snoopconsulting.com>
2023-06-21 11:51:06 -0300

    Empiezo a agregar capas a la aplicación.

---
+++ ValkimiaSolution/Valkimia.Domain/Models/Cliente.cs
@@ -0,0 +1,18 @@
+﻿#nullable disable
+
+namespace Valkimia.Domain.Models
+{
+    public class Cliente
+    {
+        public Guid Id { get; set; }
+        public string Nombre { get; set; } = null!;
+        public string Apellido { get; set; } = null!;
+        public string Domicilio { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public Guid IdCiudad { get; set; }
+        public bool Habilitado { get; set; }
+
+        public virtual Ciudad IdCiudadNavigation { get; set; }
+    }
+}
---
+++ ValkimiaSolution/Valkimia.Domain/Models/Factura.cs
@@ -0,0 +1,14 @@
+﻿#nullable disable
+
+namespace Valkimia.Domain.Models
+{
+    public class Factura
+    {
+        public Guid Id { get; set; }
+        public Guid IdCliente { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Detalle { get; set; } = null!;
+        
+        public virtual Cliente IdClienteNavigation { get; set; }
+    }
+}
